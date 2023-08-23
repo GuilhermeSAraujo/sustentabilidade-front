ad7bce03ad048aed421c21b938dcf8ad4fd2b05e
Guilherme Souza Araújo <guilhermesouzae21@gmail.com>
2023-08-22 23:57:56 -0300

    chore: removing logs

--- src/pages/Home/view/home.tsx
+++ src/pages/Home/view/home.tsx
@@ -6,8 +6,6 @@ const Home = () => {
   return (
     <Box>
       <Typography variant='h1'>Olá {user?.name}</Typography>
-      <Typography variant='body1'>Teste 1</Typography>
-      <Typography variant='body1'>{JSON.stringify(import.meta.env)}</Typography>
     </Box>
   );
 };
