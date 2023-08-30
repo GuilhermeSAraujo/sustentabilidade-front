53f61666c3751f632012b380e99feb2e73523c56
GabsuH <1338878@sga.pucminas.br>
2023-08-29 23:16:46 -0300

    style: css

--- src/pages/Products/view/products.tsx
+++ src/pages/Products/view/products.tsx
@@ -6,48 +6,59 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { Box } from '@mui/material';
 
-function createData(
-  image: string,
-  name: string,
-  expirationDate: Date,
-  registrationDate: Date,
-) {
-  return { image, name, expirationDate, registrationDate };
-}
-
-const rows = [
-  createData('Frozen yoghurt', "Garrafa de agua", new Date("2015-03-25"), new Date()),
-  createData('Ice cream sandwich', "Pão de forma", new Date("2015-03-25"), new Date()),
-  createData('Eclair', "Suco de uva", new Date("2015-03-25"), new Date()),
-  createData('Cupcake', "Bala", new Date("2015-03-25"), new Date()),
-  createData('Gingerbread', "Sucrilhos", new Date("2015-03-25"), new Date()),
+const produtos = [
+  {
+    image: "https://www.jprembalagemsp.com.br/imagens/embalagem/embalagem-para-arroz-5kg-no-jardim-monte-kemel.jpg",
+    name: 'Guilherme',
+    expirationDate: new Date(),
+    registrationDate: new Date(),
+  },
+  {
+    image: "https://www.jprembalagemsp.com.br/imagens/embalagem/embalagem-para-arroz-5kg-no-jardim-monte-kemel.jpg",
+    name: 'Gabriel',
+    expirationDate: new Date(),
+    registrationDate: new Date(),
+  },
+  {
+    image: "https://www.jprembalagemsp.com.br/imagens/embalagem/embalagem-para-arroz-5kg-no-jardim-monte-kemel.jpg",
+    name: 'Roberta',
+    expirationDate: new Date(),
+    registrationDate: new Date(),
+  },
+  {
+    image: "https://www.jprembalagemsp.com.br/imagens/embalagem/embalagem-para-arroz-5kg-no-jardim-monte-kemel.jpg",
+    name: 'Elvis',
+    expirationDate: new Date(),
+    registrationDate: new Date(),
+  },
 ];
 
 export default function Products() {
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <Table sx={{ minWidth: 650, backgroundColor: 'beige' }} aria-label="simple table">
         <TableHead>
           <TableRow>
             <TableCell>Imagem produto</TableCell>
             <TableCell align="right">Nome</TableCell>
-            <TableCell align="right">Data de Vencimento&nbsp;(g)</TableCell>
-            <TableCell align="right">Data de Cadastro&nbsp;(g)</TableCell>
+            <TableCell align="right">Data de Vencimento</TableCell>
+            <TableCell align="right">Data de Cadastro</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {produtos.map((produto, index) => (
             <TableRow
-              key={row.image}
+              key={index}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {row.image}
+                <Box component='img' src={produto.image} sx={{ maxWidth: '15%', borderRadius: '15px'}} />
               </TableCell>
-              <TableCell align="right">{row.name}</TableCell>
-              <TableCell align="right">{row.expirationDate.toLocaleDateString()}</TableCell>
-              <TableCell align="right">{row.registrationDate.toLocaleDateString()}</TableCell>
+              <TableCell align="right">{produto.name}</TableCell>
+              <TableCell align="right">{produto.expirationDate.toLocaleDateString()}</TableCell>
+              <TableCell align="right">{produto.registrationDate.toLocaleDateString()}</TableCell>
             </TableRow>
           ))}
         </TableBody>
