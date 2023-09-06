91359893772c6533f884c49caceb8780f2b3bde1
Guilherme Souza Araújo <guilhermesouzae21@gmail.com>
2023-09-05 21:13:54 -0300

    refactor: changing inline import

--- src/pages/Products/view/products.tsx
+++ src/pages/Products/view/products.tsx
@@ -1,11 +1,4 @@
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { Box, TextField } from '@mui/material';
+import { Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 
 const produtos = [
   {
