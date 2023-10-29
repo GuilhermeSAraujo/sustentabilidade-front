183932311851a4ceb412c44a4e192fc26f3ca9c4
Guilherme Souza Araújo <guilhermesouzae21@gmail.com>
2023-10-29 14:55:29 -0300

    chore: lib test for barcode

--- src/pages/Home/components/barCodeScanner.tsx
+++ src/pages/Home/components/barCodeScanner.tsx
@@ -1,82 +1,24 @@
-import Quagga from "quagga";
-import { useEffect, useState } from "react";
-const BarcodeScanner = () => {
+import { useState } from "react";
+import { useZxing } from "react-zxing";
 
-    const [isInitialised, setIsInitialised] = useState(false);
-    const onDetected = (obj : unknown) => {
-        window.alert(obj);
-    };
-    useEffect(() => {
-      Quagga.init(
-        {
-          inputStream: {
-            type: "LiveStream",
-            constraints: {
-              width: 640,
-              height: 320,
-              facingMode: "environment",
-            },
-            // area: { // defines rectangle of the detection/localization area
-            //   top: "10%",    // top offset
-            //   right: "10%",  // right offset
-            //   left: "10%",   // left offset
-            //   bottom: "10%"  // bottom offset
-            // },
-          },
-          locator: {
-            halfSample: true,
-            patchSize: "large", // x-small, small, medium, large, x-large
-            debug: {
-              showCanvas: true,
-              showPatches: false,
-              showFoundPatches: false,
-              showSkeleton: false,
-              showLabels: false,
-              showPatchLabels: false,
-              showRemainingPatchLabels: false,
-              boxFromPatches: {
-                showTransformed: true,
-                showTransformedBox: true,
-                showBB: true,
-              },
-            },
-          },
-          numOfWorkers: 4,
-          decoder: {
-            readers: ["code_128_reader"],
-            debug: {
-              drawBoundingBox: true,
-              showFrequency: true,
-              drawScanline: true,
-              showPattern: true,
-            },
-          },
-          locate: true,
-        },
-        (err : unknown) => {
-          if (err) {
-            console.log(err);
-            return;
-          }
-          Quagga.start();
-          setIsInitialised(true);
-        }
-      );
-   
-      Quagga.onDetected((result : unknown) => {
-        onDetected(result);
-      });
-   
-      return () => {
-        Quagga.offDetected((result: unknown) => {
-          onDetected(result);
-        });
-      };
-    }, [onDetected]);
-   
-    return isInitialised ? (
-      <div id="interactive" className="viewport" />
-    ) : null;
+export const BarcodeScanner = () => {
+  const [result, setResult] = useState("");
+  const { ref } = useZxing({
+    onDecodeResult(result) {
+      setResult(result.getText());
+      window.alert("Código de barras lido com sucesso: " + result.getText());
+    },
+  });
+
+  return (
+    <>
+      <video ref={ref} />
+      <p>
+        <span>Last result:</span>
+        <span>{result}</span>
+      </p>
+    </>
+  );
 };
 
 export default BarcodeScanner;
