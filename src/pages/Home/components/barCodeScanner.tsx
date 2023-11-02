122177dd1f360bd8c169e3b133fb4859fc8b2f0f
Guilherme Souza Araújo <guilherme.souza@dtidigital.com.br>
2023-11-01 21:05:41 -0300

    chore: swtching library to scan

--- src/pages/Home/components/barCodeScanner.tsx
+++ src/pages/Home/components/barCodeScanner.tsx
@@ -1,23 +1,56 @@
-import { useState } from "react";
-import { useZxing } from "react-zxing";
+import { Html5Qrcode } from 'html5-qrcode';
+import { Html5QrcodeError, QrcodeResult } from 'html5-qrcode/esm/core';
+import { useEffect, useRef } from 'react';
 
-export const BarcodeScanner = () => {
-  const [result, setResult] = useState("");
-  const { ref } = useZxing({
-    onDecodeResult(result) {
-      setResult(result.getText());
-      window.alert("Código de barras lido com sucesso: " + result.getText());
-    },
-  });
+// const qrcodeRegionId = "html5qr-code-full-region";
+
+interface BarcodeScannerProps {
+  onResult: (result: QrcodeResult) => void;
+  onError: (error: Html5QrcodeError) => void;
+}
+export const BarcodeScanner = ({onResult, onError} : BarcodeScannerProps) => {
+  const previewRef = useRef<HTMLDivElement>(null);
+  
+  const memoizedResultHandler = useRef(onResult);
+  const memoizedErrorHandler = useRef(onError);
+
+  useEffect(() => {
+    memoizedResultHandler.current = onResult;
+  }, [onResult]);
+
+  useEffect(() => {
+    memoizedErrorHandler.current = onError;
+  }, [onError]);
+
+  useEffect(() => {
+    if (!previewRef.current) return;
+    const html5QrcodeScanner = new Html5Qrcode(previewRef.current.id);
+    const didStart = html5QrcodeScanner
+      .start(
+        { facingMode: { exact: "environment" } },
+        { fps: 10 },
+        (_, { result }) => {
+          memoizedResultHandler.current(result);
+        },
+        (_, error) => {
+          memoizedErrorHandler.current(error);
+        }
+      )
+      .then(() => true);
+    return () => {
+      didStart
+        .then(() => html5QrcodeScanner.stop())
+        .catch(() => {
+          console.log('Error stopping scanner');
+        });
+    };
+  }, [previewRef, memoizedResultHandler, memoizedErrorHandler]);
 
   return (
-    <>
-      <video ref={ref} />
-      <p>
-        <span>Last result:</span>
-        <span>{result}</span>
-      </p>
-    </>
+    <div
+      id="preview"
+      ref={previewRef}
+    />
   );
 };
 
--- src/pages/Home/components/modalAddProduct.tsx
+++ src/pages/Home/components/modalAddProduct.tsx
@@ -1,10 +1,16 @@
 import { Box, Button, Modal } from "@mui/material";
 import { useState } from "react";
 import BarcodeScanner from "./barCodeScanner";
+import { QrcodeResult } from "html5-qrcode/esm/core";
 
 const ModalAddProduct = () => {
   const [modalOpen, setModalOpen] = useState(false);
+  const [barcode, setBarcode] = useState('');
 
+  const handleSuccessScan = (result : QrcodeResult) => {
+    setBarcode(result.text);
+    setModalOpen(false);
+  };
 
   return (
     <>
@@ -15,8 +21,10 @@ const ModalAddProduct = () => {
         aria-describedby="modal-modal-description"
       >
         <Box sx={modalStyle}>
-        <h6>Leitor de Código de Barras</h6>
-        <BarcodeScanner />
+          <h6>Leitor de Código de Barras</h6>
+          <h6>Valor do código de barras: {barcode}</h6>
+          <BarcodeScanner onResult={handleSuccessScan}
+          onError={(result) => setBarcode(result.errorMessage)} />
         </Box>
       </Modal>
       <Button variant="contained" onClick={() => setModalOpen(true)}>
