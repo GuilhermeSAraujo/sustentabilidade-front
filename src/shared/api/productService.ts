7e65c327bb817b597f4dd2b169e6e1bf9ca13352
Guilherme Souza Araújo <guilhermesouzae21@gmail.com>
2023-11-21 20:57:23 -0300

    chore: changing endpoint

--- src/shared/api/productService.ts
+++ src/shared/api/productService.ts
@@ -5,27 +5,11 @@ import { formatDate } from "../utils/formUtils";
 
 class ProductService {
     getProductByBarcode = async (barcode: string) => {
-        /*const product = {
-            barcode: barcode,
-            // barcode: "7896070511019",
-            name: "PULLMAN TRADICIONAL",
-            description:
-                "Preparações à base de cereais, farinhas, amidos, féculas ou de leite; produtos de pastelaria - Produtos de padaria, pastelaria ou da indústria de bolachas e biscoitos, mesmo adicionados de cacau; hóstias, cápsulas vazias para medicamentos, obreias, pastas secas de farinha, amido ou fécula, em folhas, e produtos semelhantes -  Outros - Pão de forma",
-            brand: null,
-            average_price: 18.99,
-            image_url: "https://cdn-cosmos.bluesoft.com.br/products/7896070511019",
-        } as any;*/
         const user = localStorage.getItem('user');
         if (!user) throw new Error('Usuário não logado');
         const userLoggedIn = JSON.parse(user) as UserGetResult;
-        const product = await axios.get<ProductDataGetResult>(`http://localhost:9999/api/product?searchable_field=barcode&value=${barcode}`, { headers: { Authorization: `Bearer ${userLoggedIn.token}` } });
+        const product = await axios.get<ProductDataGetResult>(`https://helloelitxmq.com/api/product?searchable_field=barcode&value=${barcode}`, { headers: { Authorization: `Bearer ${userLoggedIn.token}` } });
 
-        /*await new Promise<void>((resolve) =>
-            setTimeout(() => {
-                resolve(product);
-            }, 2500)
-        );*/
-        // return product;
         return product.data;
     };
 
@@ -50,7 +34,7 @@ class ProductService {
             user_id: userLoggedIn.id,
         }
 
-        await axios.post("http://localhost:9999/api/product/assign", payload, { headers: { Authorization: `Bearer ${userLoggedIn.token}` } });
+        await axios.post("https://helloelitxmq.com/api/product/assign", payload, { headers: { Authorization: `Bearer ${userLoggedIn.token}` } });
     };
 
     getUserProducts = async (): Promise<IUsersProducts> => {
@@ -61,7 +45,7 @@ class ProductService {
         const userLoggedIn = JSON.parse(user) as UserGetResult;
 
         const response = await axios.get<UsersProductsGetResult>(
-            `http://localhost:9999/api/product/user/${userLoggedIn.id}`,
+            `https://helloelitxmq.com/api/product/user/${userLoggedIn.id}`,
             { headers: { Authorization: `Bearer ${userLoggedIn.token}` } });
 
         return {
--- src/shared/api/userService.ts
+++ src/shared/api/userService.ts
@@ -3,13 +3,13 @@ import { ISignIn, ISignUp, UserGetResult } from "../../models/user";
 
 class UserService {
     create = async (data: ISignUp) => {
-        await axios.post("http://localhost:9999/api/user/register", data, {
+        await axios.post("https://helloelitxmq.com/api/user/register", data, {
             timeout: 5000,
         });
     };
 
     auth = async (data: ISignIn): Promise<UserGetResult> => {
-        const { data: user } = await axios.post<UserGetResult>("http://localhost:9999/api/user/authenticate", data, {
+        const { data: user } = await axios.post<UserGetResult>("https://helloelitxmq.com/api/user/authenticate", data, {
             timeout: 5000,
         });
         return user;
